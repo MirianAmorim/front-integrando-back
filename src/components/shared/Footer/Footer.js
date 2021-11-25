e5d24ca30f5879732eb99fef1daffd5f3ae3bb88
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2021-11-24 23:43:21 -0300

    front-integrando-back
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

---
+++ src/components/shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="w-100">
+      <div className="container">
+        <p className="text-center"> Front desenvolvido por Blue EdTech e MirianAmorim</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
